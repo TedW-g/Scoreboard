cccd8bf634dd1793170fd3e17ad481cbf80395e7
TedW-g <tedw3@nycstudents.net>
2024-09-25 13:32:37 +0000

    commit

--- Scoreboard.java
+++ Scoreboard.java
@@ -1,4 +1,10 @@
-public class Scoreboard
+public class Scoreboard 
 {
-    private int 
+    private String team1;
+    private String team2;
+}
+
+public Scoreboard(String one, String two)
+{
+    team1
 }
