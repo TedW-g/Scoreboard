ee473fabba3c5b6166faf7da7ca6d5e5e218f3fa
TedW-g <tedw3@nycstudents.net>
2024-10-07 13:32:45 +0000

    commit

--- Main.java
+++ Main.java
@@ -4,7 +4,26 @@ public class Main
     {
         String info;
         Scoreboard game = new Scoreboard("Red", "Blue");
+
+        //test
+
+        System.out.println(game.getScore());
+
+        //test1
+
         game.recordPlay(1);
+        info = game.getScore();
+        System.out.println(info);
+
+        //test2
+
+        game.recordPlay(1);
+        info = game.getScore();
+        System.out.println(info);
+
+        //test3
+
+        game.recordPlay(0);
         info = game.getScore();
         System.out.println(info);
     }
